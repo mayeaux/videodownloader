ecca10c49155bcf9c4f1920fa0565aca7fd05d90
Anthony <contact@anthonymayfield.com>
2019-06-27 19:48:17 -0600

    almost downloading channel

--- renderer.js
+++ renderer.js
@@ -100,8 +100,9 @@ function download(url, title, downloadAsAudio, youtubeUrl, saveAsTitleValue){
 
     console.log(`child process exited with code ${code}`);
   });
-
 }
+
+
 // start download button
 var startDownload = document.getElementsByClassName('startDownload')[0];
 
@@ -138,9 +139,102 @@ startDownload.onclick = function(){
 
 };
 
+// var url = 'https://www.youtube.com/watch?v=ZcAiayke00I';
+function downloadChannel(url, title, downloadAsAudio, youtubeUrl, saveAsTitleValue){
+
+  let arguments = [];
+
+  // set the url for ytdl
+  arguments.push(url);
+
+  // verbose output
+  arguments.push('-v');
+
+  // keep video
+  arguments.push('-k');
+
+  // arguments.push('-f', 'bestvideo+bestaudio/best');
+
+  arguments.push('-f');
 
 
+  // select download as audio or video
+  if(downloadAsAudio){
+    arguments.push('bestaudio');
+  } else {
+    arguments.push('bestvideo');
+  }
 
+  // // verbose output
+
+  // title is that passed or the one from youtube
+  const fileName = title || '%(title)s';
+
+  // const filePath = '/Users/anthony/Development/ytdldesktop/thinger/electron-quick-start/videos/Keemstar';
+
+  const filePath = __dirname + '/videos';
+
+
+  // save to videos directory
+  arguments.push('-o', `${filePath}/${fileName}.%(ext)s`);
+
+  console.log(arguments);
+
+  // download as audio if needed
+  if(downloadAsAudio){
+    console.log('Download as audio');
+    arguments.push('-x');
+  }
+
+  const ls = spawn('node_modules/youtube-dl/bin/youtube-dl', arguments);
+
+  ls.stdout.on('data', (data) => {
+    percentage.innerText = data;
+
+
+    console.log(`stdout: ${data}`);
+  });
+
+  ls.stderr.on('data', (data) => {
+    percentage.innerText = data;
+
+
+    console.log(`stderr: ${data}`);
+  });
+
+  ls.on('close', (code) => {
+
+    youtubeUrl.value = '';
+    saveAsTitleValue.value = '';
+
+    // downloaded++;// if(downloaded == needsToDownload){
+    //   downloading = false;
+    //   console.log('no longer downloading');
+    // }
+
+    if(code == 0){
+      percentage.innerText = 'Download completed';
+    }
+
+    console.log(`child process exited with code ${code}`);
+  });
+}
+
+
+var startChannelDownload =  document.getElementsByClassName('startChannelDownload')[0];
+
+
+startChannelDownload.onclick = function(){
+
+  var youtubeChannelUrl = document.getElementsByClassName('youtubeChannelUrl')[0];
+  var downloadChannelAsAudio =document.getElementsByClassName('downloadChannelAsAudio')[0];
+
+  var youtubeChannelUrlValue = youtubeChannelUrl.value;
+  var downloadChannelAsAudioValue = downloadChannelAsAudio.checked;
+
+  downloadChannel(youtubeChannelUrl, youtubeChannelUrlValue, downloadChannelAsAudioValue, youtubeChannelUrl, '');
+
+};
 
 
 // frontend code
